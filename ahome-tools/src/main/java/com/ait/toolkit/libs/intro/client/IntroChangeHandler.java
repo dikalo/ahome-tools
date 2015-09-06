287ad11e56f7b0d42af05b2753734b1c5308f497
eemi210 <ekambi@ahome-it-com>
2015-09-07 00:22:24 +0200

    removes intro js which is now part of gwt-tour

--- ahome-tools/src/main/java/com/ait/toolkit/libs/intro/client/IntroChangeHandler.java
+++
@@ -1,7 +0,0 @@
-package com.ait.toolkit.libs.intro.client;
-
-import com.google.gwt.dom.client.Element;
-
-public interface IntroChangeHandler {
-	public void onEvent(Element element);
-}
--- ahome-tools/src/main/java/com/ait/toolkit/libs/intro/client/TooltipPosition.java
+++
@@ -1,5 +0,0 @@
-package com.ait.toolkit.libs.intro.client;
-
-public enum TooltipPosition {
-	TOP, RIGHT, BOTTOM, LEFT, BOTTOM_MIDDLE_ALIGNED, BOTTOM_LEFT_ALIGNED, BOTTOM_RIGHT_ALIGNED;
-}
